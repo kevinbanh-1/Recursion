9cdfe95927186abedc96d2924b13fa5de931a08b
KEVIN BANH <kevin.n.banh@ou.edu>
2020-11-09 16:27:31 -0600

    Added Cake and EatCake classes and methods

---
+++ src/EatCake.java
@@ -0,0 +1,21 @@
+
+public class EatCake {
+	
+	public EatCake() {
+		
+	}
+	
+	
+	public static void eatCake(Cake cake) {
+		if(!cake.isEmpty()) {
+			cake.takeABite();
+			eatCake(cake);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Cake cake = new Cake();
+		eatCake(cake);
+		System.out.println("Done eating the cake");
+	}
+}
